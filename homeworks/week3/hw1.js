4f3028b268f9d994151a391a7e9f00fddc97d2a5
George <twlin.george@gmail.com>
2019-05-04 17:30:38 +0800

    finish my week3

--- homeworks/week3/hw1.js
+++ homeworks/week3/hw1.js
@@ -1,5 +1,12 @@
 function stars(n) {
-  console.log(n);
-}
+  const arry = [];
+  let star = '';
 
+  for (let i = 0; i < n; i += 1) {
+    star += '*';
+    arry.push(star);
+  }
+  return arry;
+}
+console.log(stars(3));
 module.exports = stars;
--- homeworks/week3/hw4.js
+++ homeworks/week3/hw4.js
@@ -1,5 +1,20 @@
 function isPalindromes(str) {
-  console.log(str);
+  let same = '';
+  let same1 = '';
+  for (let i = 0; i < str.length; i += 1) {
+    same = same + str.charAt(i) + str.charAt(str.length - i - 1);
+  }
+  for (let g = 0; g < str.length; g += 1) {
+    same1 += str.charAt(g).repeat(2);
+  }
+  if (same === same1) {
+    return true;
+  }
+  return false;
 }
 
+console.log(isPalindromes('abcba'));
+console.log(isPalindromes('apple'));
+console.log(isPalindromes('aaaaa'));
+console.log(isPalindromes('applppa'));
 module.exports = isPalindromes;
