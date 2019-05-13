1ac60858093bc107a5ddf3c2fbaadf0726909e1c
George <twlin.george@gmail.com>
2019-05-13 19:12:22 +0800

    correction

--- homeworks/week4/hw1.js
+++ homeworks/week4/hw1.js
@@ -1,9 +1,8 @@
 const request = require('request');
 
-let n = 0;
-
 request('https://lidemy-book-store.herokuapp.com/books?_limit=10',
   (error, response, body) => {
+    let n = 0;
     const json = JSON.parse(body);
     while (n < 10) {
       console.log(`${json[n].id} ${json[n].name}`);
